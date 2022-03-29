8bb18f84e80a1642f8cb43db3e51e6ede52fbd1b
Jingxuan Feng <henryfeng24@pm.me>
2022-03-29 15:14:26 +1100

    Finished Exercise1

--- Programming Challenge/Exercise1.py
+++ Programming Challenge/Exercise1.py
@@ -3,4 +3,21 @@
 # Student name: Jingxuan Feng
 # Student number: s3843790
 
-# TODO: Implement
+# Get inputs from user
+username = input("What is your name?")
+input_float = float(input("Enter a float:"))
+input_integer = int(input("Enter a integer:"))
+
+
+# Function for calculating product
+def product(float_value, integer_value):
+    return float_value * integer_value
+
+
+# Use function & get result
+product_result = product(input_float, input_integer)
+
+
+# Print according to spec requirements
+print("Hello " + username + " !!!")
+print("The product is:", product_result)
